230d46086cfbb9978387c84ffe6846eb5d2d85c8
Hyeseong Kim <hey@hyeseong.kim>
2024-11-09 09:48:15 +0900

    adjust parens

--- tests/tests/src/variantsMatching.mjs
+++ tests/tests/src/variantsMatching.mjs
@@ -337,7 +337,7 @@ let MyNullableExtended = {
 function area(shape) {
   switch (shape.kind) {
     case 1 :
-      return Math.PI * (shape.radius ** 2);
+      return Math.PI * shape.radius ** 2;
     case "square" :
       return shape.sideLength ** 2;
     case "rectangle" :
