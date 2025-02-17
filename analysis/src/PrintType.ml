9ffe136abef18353813387efe498b71566a9272b
Gabriel Nordeborn <gabbe.nord@gmail.com>
2025-02-17 20:18:41 +0100

    also change decl printing to get rid of Stdlib prefix

--- analysis/src/PrintType.ml
+++ analysis/src/PrintType.ml
@@ -6,6 +6,13 @@ let printExpr ?(lineWidth = 60) typ =
 
 let printDecl ?printNameAsIs ~recStatus name decl =
   Printtyp.reset_names ();
+  let name =
+    if
+      String.starts_with ~prefix:"Stdlib_" name
+      || String.starts_with ~prefix:"Stdlib." name
+    then String.sub name 7 (String.length name - 7)
+    else name
+  in
   Res_doc.to_string ~width:60
     (Res_outcome_printer.print_out_sig_item_doc ?print_name_as_is:printNameAsIs
        (Printtyp.tree_of_type_declaration (Ident.create name) decl recStatus))
