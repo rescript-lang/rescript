ff710cde3168756f97be89ec4fd32194b14692cb
Hyeseong Kim <hey@hyeseong.kim>
2025-03-26 05:06:38 +0900

    update artifacts

--- .github/workflows/get_artifact_dir_name.js
+++ .github/workflows/get_artifact_dir_name.js
@@ -1,7 +1,7 @@
 import * as fs from "node:fs";
 import * as os from "node:os";
 
-import { platformName } from "#cli/paths";
+import { platformName } from "#cli/bins";
 
 // Pass artifactDirName to subsequent GitHub actions
 fs.appendFileSync(
