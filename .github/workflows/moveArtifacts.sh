25571d73c410e11a4cbc388b5efad1a0e050efa7
Hyeseong Kim <hey@hyeseong.kim>
2025-04-17 05:37:28 +0900

    upload platform artifacts

---
+++ .github/workflows/moveArtifacts.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -e
+
+check_statically_linked() {
+    local dir=$1
+    local all_statically_linked=true
+
+    for file in "$dir"/*; do
+        if [ -f "$file" ]; then
+            if file "$file" | grep -Eq "statically linked|static-pie linked"; then
+                echo "$file is statically linked."
+            else
+                echo "$file is NOT statically linked."
+                all_statically_linked=false
+            fi
+        fi
+    done
+
+    if $all_statically_linked; then
+        echo "All files in $dir are statically linked executables."
+    else
+        echo "Error: Not all files in $dir are statically linked executables."
+        exit 1
+    fi
+}
+
+# rescript
+mv lib-ocaml lib/ocaml
+
+# @rescript/{target}
+chmod +x binaries-*/*.exe rewatch-*/*.exe
+mv -f binaries-darwin/* rewatch-darwin/*           packages/@rescript/darwin-x64/bin
+mv -f binaries-darwinarm64/* rewatch-darwinarm64/* packages/@rescript/darwin-arm64/bin
+mv -f binaries-linux/* rewatch-linux/*             packages/@rescript/linux-x64/bin
+mv -f binaries-linuxarm64/* rewatch-linuxarm64/*   packages/@rescript/linux-arm64/bin
+mv -f binaries-win32/* rewatch-win32/*             packages/@rescript/win32-x64/bin
+check_statically_linked "packages/@rescript/linux-x64/bin"
+check_statically_linked "packages/@rescript/linux-arm64/bin"
+
+# @rescript/std
+mkdir -p packages/std/lib
+cp -R lib/es6 lib/js packages/std/lib
--- .github/workflows/prepare_package_upload.js
+++ .github/workflows/prepare_package_upload.js
@@ -10,13 +10,38 @@ const { version } = packageSpec;
 const commitHash = process.argv[2] || process.env.GITHUB_SHA;
 const commitHashShort = commitHash.substring(0, 7);
 
+// rescript
 fs.renameSync(
-  `rescript-${version}.tgz`,
+  "package.tgz",
   `rescript-${version}-${commitHashShort}.tgz`,
 );
+
+// @rescript/std
 fs.renameSync(
-  `packages/std/rescript-std-${version}.tgz`,
+  "packages/@rescript/std/package.tgz",
   `rescript-std-${version}-${commitHashShort}.tgz`,
+);
+
+// @rescript/{target}
+fs.renameSync(
+  "packages/@rescript/linux-x64/package.tgz",
+  `rescript-linux-x64-${version}-${commitHashShort}.tgz`,
+);
+fs.renameSync(
+  "packages/@rescript/linux-arm64/package.tgz",
+  `rescript-linux-arm64-${version}-${commitHashShort}.tgz`,
+);
+fs.renameSync(
+  "packages/@rescript/darwin-x64/package.tgz",
+  `rescript-darwin-x64-${version}-${commitHashShort}.tgz`,
+);
+fs.renameSync(
+  "packages/@rescript/darwin-arm64/package.tgz",
+  `rescript-darwin-arm64-${version}-${commitHashShort}.tgz`,
+);
+fs.renameSync(
+  "packages/@rescript/win32-x64/package.tgz",
+  `rescript-win32-x64-${version}-${commitHashShort}.tgz`,
 );
 
 // Pass information to subsequent GitHub actions
