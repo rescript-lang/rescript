8c4d743983ac19b5ae006e01222a25f5d4aae30a
Hyeseong Kim <hey@hyeseong.kim>
2025-04-17 06:36:30 +0900

    expose bsb_helper path

--- packages/@rescript/win32-x64/bin.js
+++ packages/@rescript/win32-x64/bin.js
@@ -5,6 +5,7 @@ import * as path from "node:path";
 export const binDir = path.join(import.meta.dirname, "bin");
 
 export const binPaths = {
+  bsb_helper_exe: path.join(binDir, "bsb_helper.exe"),
   bsc_exe: path.join(binDir, "bsc.exe"),
   ninja_exe: path.join(binDir, "ninja.exe"),
   rescript_exe: path.join(binDir, "rescript.exe"),
