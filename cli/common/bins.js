59c41fc77914024353eef27e6f4158921ebc69d2
Hyeseong Kim <hey@hyeseong.kim>
2025-04-17 07:25:26 +0900

    compute realpath of binaries as needed

--- cli/common/bins.js
+++ cli/common/bins.js
@@ -1,5 +1,7 @@
 // @ts-check
 
+import * as fs from "node:fs";
+
 /**
  * @typedef {{
  *   binDir: string,
@@ -27,15 +29,29 @@ try {
   throw new Error(`Platform ${target} is not supported!`);
 }
 
+const binPaths = mod.binPaths;
+// Compiler use symbolic links in the development process
+if (fs.lstatSync(binPaths.rescript_exe).isSymbolicLink()) {
+  for (const [key, binPath] of Object.entries(binPaths)) {
+    try {
+      binPaths[/** @type {keyof BinaryPaths} */ (key)] =
+        fs.realpathSync(binPath);
+    } catch {
+      // Cannot populate the realpath of a binary, some tests may fails.
+      //
+      // But we don't report error here,
+      // so pass even some binaries are not built yet.
+    }
+  }
+}
+
+export const platformDir = mod.binDir;
 export const {
-  binDir: platformDir,
-  binPaths: {
-    bsb_helper_exe,
-    bsc_exe,
-    ninja_exe,
-    rescript_editor_analysis_exe,
-    rescript_tools_exe,
-    rescript_exe,
-    rewatch_exe,
-  },
-} = mod;
+  bsb_helper_exe,
+  bsc_exe,
+  ninja_exe,
+  rescript_editor_analysis_exe,
+  rescript_tools_exe,
+  rescript_exe,
+  rewatch_exe,
+} = binPaths;
