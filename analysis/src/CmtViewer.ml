23443579935fd9c9b038695f34d9e4803471fa10
nojaf <florian.verdonck@outlook.com>
2025-04-26 13:47:35 +0200

    Dump locItems

--- analysis/src/CmtViewer.ml
+++ analysis/src/CmtViewer.ml
@@ -1,5 +1,5 @@
 let loc_to_string (loc : Warnings.loc) : string =
-  Format.sprintf "(%d,%d--%d,%d)" loc.loc_start.pos_lnum
+  Format.sprintf "(%03d,%03d--%03d,%03d)" loc.loc_start.pos_lnum
     (loc.loc_start.pos_cnum - loc.loc_start.pos_bol)
     loc.loc_end.pos_lnum
     (loc.loc_end.pos_cnum - loc.loc_end.pos_bol)
@@ -8,6 +8,7 @@ let dump path =
   match Cmt.loadFullCmtFromPath ~path with
   | None -> failwith (Format.sprintf "Could not load cmt for %s" path)
   | Some full ->
+    let open SharedTypes in
     let open SharedTypes.Stamps in
     let stamps = full.file.stamps |> getEntries in
 
@@ -15,8 +16,8 @@ let dump path =
 
     stamps
     |> List.sort (fun (_, a) (_, b) ->
-           let aLoc = SharedTypes.Stamps.locOfKind a in
-           let bLoc = SharedTypes.Stamps.locOfKind b in
+           let aLoc = locOfKind a in
+           let bLoc = locOfKind b in
            match compare aLoc.loc_start.pos_lnum bLoc.loc_start.pos_lnum with
            | 0 -> compare aLoc.loc_start.pos_cnum bLoc.loc_start.pos_cnum
            | c -> c)
@@ -33,4 +34,25 @@ let dump path =
                (loc_to_string t.extentLoc)
            | KConstructor t ->
              Printf.printf "%d kconstructor %s\n" stamp
-               (loc_to_string t.extentLoc))
+               (loc_to_string t.extentLoc));
+
+    (* Dump all locItems (typed nodes) *)
+    let locItems =
+      match full.extra with
+      | {locItems} -> locItems
+    in
+
+    Printf.printf "\nFound %d locItems (typed nodes):\n\n"
+      (List.length locItems);
+
+    locItems
+    |> List.sort (fun a b ->
+           let aLoc = a.loc.Location.loc_start in
+           let bLoc = b.loc.Location.loc_start in
+           match compare aLoc.pos_lnum bLoc.pos_lnum with
+           | 0 -> compare aLoc.pos_cnum bLoc.pos_cnum
+           | c -> c)
+    |> List.iter (fun {loc; locType} ->
+           let locStr = loc_to_string loc in
+           let kindStr = SharedTypes.locTypeToString locType in
+           Printf.printf "%s %s\n" locStr kindStr)
