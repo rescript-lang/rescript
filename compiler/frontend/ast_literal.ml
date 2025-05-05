337e645d08b352641932ae3c5ef4897ccbe4798b
Gabriel Nordeborn <gabbe.nord@gmail.com>
2025-05-05 11:04:57 +0200

    change completions of regex literals

--- compiler/frontend/ast_literal.ml
+++ compiler/frontend/ast_literal.ml
@@ -71,7 +71,7 @@ module Lid = struct
   let js_null_undefined : t = Ldot (Lident "Js", "null_undefined")
 
   (* FIXME: Use primitive module *)
-  let js_re_id : t = Ldot (Ldot (Lident "Js", "Re"), "t")
+  let js_re_id : t = Ldot (Lident "RegExp", "t")
 end
 
 module No_loc = struct
