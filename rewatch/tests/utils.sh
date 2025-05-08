71b39d4338a65a3535947aa3d4042bc7978f29aa
Jaap Frolich <jfrolich@gmail.com>
2025-05-08 21:56:42 +0200

    make tests work correctly

--- rewatch/tests/utils.sh
+++ rewatch/tests/utils.sh
@@ -4,6 +4,7 @@ success() { echo -e "- ✅ \033[32m$1\033[0m"; }
 error() { echo -e "- 🛑 \033[31m$1\033[0m"; }
 bold() { echo -e "\033[1m$1\033[0m"; }
 rewatch() { RUST_BACKTRACE=1 $REWATCH_EXECUTABLE --no-timing=true $@; }
+rewatch_bg() { RUST_BACKTRACE=1 nohup $REWATCH_EXECUTABLE --no-timing=true $@; }
 
 replace() {
   if [[ $OSTYPE == 'darwin'* ]];
