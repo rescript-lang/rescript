1119f59be798d8607366b0520a67c922832169a2
Hyeseong Kim <hey@hyeseong.kim>
2025-05-18 12:12:13 +0900

    constraints for other meta

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -12,13 +12,37 @@ const { compilerVersionFile } = require("#dev/paths");
 /**
  * @param {Yarn.Constraints.Context} ctx
  */
-async function enforceCompilerVersion({ Yarn }) {
+async function enforceCompilerMeta({ Yarn }) {
   const EXPECTED_VERSION = "12.0.0-alpha.13";
 
   for (const workspace of Yarn.workspaces()) {
     const { ident } = workspace.pkg;
     if (ident === "rescript" || ident.startsWith("@rescript/")) {
       workspace.set("version", EXPECTED_VERSION);
+      workspace.set("homepage", "https://rescript-lang.org");
+      workspace.set("bugs", "https://github.com/rescript-lang/rescript/issues");
+      workspace.set("repository", {
+        type: "git",
+        url: "git+https://github.com/rescript-lang/rescript.git",
+      });
+      workspace.set("author", {
+        name: "Hongbo Zhang",
+        email: "bobzhang1988@gmail.com",
+      });
+      workspace.set("maintainers", [
+        "Christoph Knittel (https://github.com/cknitt)",
+        "Cristiano Calcagno (https://github.com/cristianoc)",
+        "Dmitry Zakharov (https://github.com/DZakh)",
+        "Florian Hammerschmidt (https://github.com/fhammerschmidt)",
+        "Gabriel Nordeborn (https://github.com/zth)",
+        "Hyeseong Kim (https://github.com/cometkim)",
+        "Jaap Frolich (https://github.com/jfrolich)",
+        "Matthias Le Brun (https://github.com/bloodyowl)",
+        "Patrick Ecker (https://github.com/ryyppy)",
+        "Paul Tsnobiladzé (https://github.com/tsnobip)",
+        "Woonki Moon (https://github.com/mununki)",
+      ]);
+      workspace.set("preferUnplugged", true);
     }
   }
 
@@ -46,6 +70,6 @@ async function enforceCompilerVersion({ Yarn }) {
 
 module.exports = defineConfig({
   async constraints(ctx) {
-    await enforceCompilerVersion(ctx);
+    await enforceCompilerMeta(ctx);
   },
 });
