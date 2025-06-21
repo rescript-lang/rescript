66974f9b7ee4254ab95652d3eb639ba40313bb5b
Bushuo <me@paulbuschmann.com>
2025-06-21 19:22:23 +0200

    fix: pass bsc-path arg only to specific subcommands

--- cli/rewatch.js
+++ cli/rewatch.js
@@ -7,6 +7,36 @@ import { rewatch_exe, bsc_exe } from "./common/bins.js";
 
 const args = process.argv.slice(2);
 
-child_process.spawnSync(rewatch_exe, [...args, "--bsc-path", bsc_exe], {
-  stdio: "inherit",
-});
+const firstPositionalArgIndex = args.findIndex((arg) => !arg.startsWith("-"));
+
+try {
+  if (firstPositionalArgIndex !== -1) {
+    const subcommand = args[firstPositionalArgIndex];
+    const subcommandArgs = args.slice(firstPositionalArgIndex + 1);
+
+    if (
+      subcommand === "build" ||
+      subcommand === "watch" ||
+      subcommand === "clean" ||
+      subcommand === "compiler-args"
+    ) {
+      child_process.execFileSync(
+        rewatch_exe,
+        [...subcommandArgs, "--bsc-path", bsc_exe],
+        {
+          stdio: "inherit",
+        }
+      );
+    } else {
+      child_process.execFileSync(rewatch_exe, [...args], {
+        stdio: "inherit",
+      });
+    }
+  }
+} catch (err) {
+  if (err.status !== undefined) {
+    process.exit(err.status); // Pass through the exit code
+  } else {
+    process.exit(1); // Generic error
+  }
+}
