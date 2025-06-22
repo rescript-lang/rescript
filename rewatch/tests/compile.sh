fb9400cedcb5aa48d2203a9e671b4fb4fdd68c55
Bushuo <me@paulbuschmann.com>
2025-06-22 20:03:07 +0200

    test: improve test output

--- rewatch/tests/compile.sh
+++ rewatch/tests/compile.sh
@@ -87,21 +87,23 @@ then
 fi
 
 file_count=$(find ./packages/with-dev-deps/test -name *.mjs | wc -l)
-if [ "$file_count" -eq 1 ];
+expected_file_count=1
+if [ "$file_count" -eq $expected_file_count ];
 then
   success "Compiled dev dependencies successfully"
 else
-  error "Expected 2 files to be compiled with the --dev flag, found $file_count"
+  error "Expected $expected_file_count files to be compiled with the --dev flag, found $file_count"
   exit 1
 fi
 
-rewatch clean &> /dev/null
+error_output=$(rewatch clean 2>&1 >/dev/null)
 file_count=$(find ./packages/with-dev-deps -name *.mjs | wc -l)
 if [ "$file_count" -eq 0 ];
 then
   success "Cleaned dev dependencies successfully"
 else
   error "Expected 0 files remaining after cleaning, found $file_count"
+  printf "%s\n" "$error_output" >&2
   exit 1
 fi
 
