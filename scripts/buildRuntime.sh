4adf7a7360bdef5504d4acb87a4ff7f3d0d2c03a
Christoph Knittel <ck@cca.io>
2025-07-03 15:42:41 +0200

    Rename runtime build scripts

--- scripts/buildRuntime.sh
+++ scripts/buildRuntime.sh
@@ -2,7 +2,7 @@
 set -e
 shopt -s extglob
 
-(cd runtime && ../cli/rescript-legacy.js clean && ../cli/rescript-legacy.js build)
+(cd runtime && ../cli/rescript.js clean && ../cli/rescript.js build)
 
 rm -f lib/es6/*.js lib/js/*.js lib/ocaml/*
 mkdir -p lib/es6 lib/js lib/ocaml
@@ -11,3 +11,6 @@ cp runtime/lib/es6/*.js lib/es6
 cp runtime/lib/js/*.js lib/js
 cp runtime/lib/bs/*.@(cmi|cmj|cmt|cmti) lib/ocaml/
 cp runtime/*.@(res|resi) lib/ocaml/
+# overwrite the stdlib build artifacts to the testrepo
+mkdir -p rewatch/testrepo/node_modules/rescript/lib/ocaml
+cp -rf lib/ocaml rewatch/testrepo/node_modules/rescript/lib/ocaml
