a423aadc66953378c088a35fe3221cc6bbadae15
Médi-Rémi Hashim <mediremi@gmail.com>
2025-07-04 19:50:17 +0100

    Run cargo fmt

--- rewatch/src/build/parse.rs
+++ rewatch/src/build/parse.rs
@@ -242,11 +242,7 @@ pub fn generate_asts(
         }
     });
 
-    if has_failure {
-        Err(stderr)
-    } else {
-        Ok(stderr)
-    }
+    if has_failure { Err(stderr) } else { Ok(stderr) }
 }
 
 pub fn parser_args(
@@ -334,26 +330,29 @@ fn generate_ast(
             .args(parser_args)
             .output()
             .expect("Error converting .res to .ast"),
-    ) { Some(res_to_ast) => {
-        let stderr = std::str::from_utf8(&res_to_ast.stderr).expect("Expect StdErr to be non-null");
-        if helpers::contains_ascii_characters(stderr) {
-            if res_to_ast.status.success() {
-                Ok((ast_path, Some(stderr.to_string())))
+    ) {
+        Some(res_to_ast) => {
+            let stderr = std::str::from_utf8(&res_to_ast.stderr).expect("Expect StdErr to be non-null");
+            if helpers::contains_ascii_characters(stderr) {
+                if res_to_ast.status.success() {
+                    Ok((ast_path, Some(stderr.to_string())))
+                } else {
+                    Err(format!("Error in {}:\n{}", package.name, stderr))
+                }
             } else {
-                Err(format!("Error in {}:\n{}", package.name, stderr))
+                Ok((ast_path, None))
             }
-        } else {
-            Ok((ast_path, None))
         }
-    } _ => {
-        log::info!("Parsing file {}...", filename.display());
+        _ => {
+            log::info!("Parsing file {}...", filename.display());
 
-        Err(format!(
-            "Could not find canonicalize_string_path for file {} in package {}",
-            filename.display(),
-            package.name
-        ))
-    }};
+            Err(format!(
+                "Could not find canonicalize_string_path for file {} in package {}",
+                filename.display(),
+                package.name
+            ))
+        }
+    };
     if let Ok((ast_path, _)) = &result {
         let _ = std::fs::copy(
             Path::new(&build_path_abs).join(&ast_path),
