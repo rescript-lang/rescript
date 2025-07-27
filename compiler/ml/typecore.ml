c03ec44c5cbddc4ed9ddbdfa74256721f3b8a1bb
Gabriel Nordeborn <gabbe.nord@gmail.com>
2025-07-27 20:41:42 +0200

    more todo comments for actions that could be useful

--- compiler/ml/typecore.ml
+++ compiler/ml/typecore.ml
@@ -2778,6 +2778,7 @@ and type_expect_ ~context ?in_function ?(recarg = Rejected) env sexp ty_expected
     in
     let opt_exp =
       if List.length lid_sexp_list = num_fields then (
+        (* TODO(actions) Remove `...` spread *)
         Location.prerr_warning loc Warnings.Useless_record_with;
         None)
       else opt_exp
@@ -4715,6 +4716,7 @@ let report_error env loc ppf error =
 
     if List.length missing_required_args > 0 then (
       (* TODO(actions) Add missing arguments *)
+      (* TODO(actions) Partially apply *)
       fprintf ppf "@,- Missing arguments that must be provided: %s"
         (missing_required_args
         |> List.map (fun v -> "~" ^ v)
