450857656e15140367049c0892a4946956673c05
Shulhi Sapli <shulhi@gmail.com>
2025-07-29 19:00:41 +0800

    Print () instead of Doc.nil

--- compiler/syntax/src/res_printer.ml
+++ compiler/syntax/src/res_printer.ml
@@ -834,8 +834,10 @@ and print_mod_type ~state mod_type cmt_tbl =
                                  print_attributes ~state attrs cmt_tbl
                                in
                                let lbl_doc =
-                                 if lbl.Location.txt = "_" || lbl.txt = "*" then
-                                   Doc.nil
+                                 if lbl.Location.txt = "_" then Doc.nil
+                                 else if lbl.txt = "*" then
+                                   let doc = Doc.text "()" in
+                                   print_comments doc cmt_tbl lbl.loc
                                  else
                                    let doc = Doc.text lbl.txt in
                                    print_comments doc cmt_tbl lbl.loc
