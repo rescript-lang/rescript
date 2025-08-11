e5b723cba1e940a2c4a273f12fdc8f1cf7de605b
nojaf <florian.verdonck@outlook.com>
2025-08-11 13:12:53 +0200

    Add test for only formatting the current project.

--- rewatch/tests/format.sh
+++ rewatch/tests/format.sh
@@ -42,3 +42,18 @@ else
     echo $error_output
     exit 1
 fi
+
+bold "Test: It should format only the current project"
+
+error_output=$(cd packages/file-casing && "../../$REWATCH_EXECUTABLE" format)
+git_diff_file_count=$(git diff --name-only ./ | wc -l | xargs)
+if [ $? -eq 0 ] && [ $git_diff_file_count -eq 2 ];
+then
+    success "file-casing formatted"
+    git restore .
+else
+    error "Error formatting current project file-casing"
+     echo "Expected 2 files to be changed, got $git_diff_file_count"
+    echo $error_output
+    exit 1
+fi
