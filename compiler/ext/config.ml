ef1d4effd3e6de0b6e363e5367bb407059181b49
Christoph Knittel <ck@cca.io>
2025-08-24 13:44:31 +0200

    Fix pnpm

--- compiler/ext/config.ml
+++ compiler/ext/config.ml
@@ -10,21 +10,22 @@ let runtime_module_path =
     |> List.rev
   with
   (* 1. Packages installed via pnpm
-     - bin:    node_modules/.pnpm/@rescript+darwin-arm64@12.0.0-alpha.13/node_modules/@rescript/darwin-arm64/bin
-     - stdlib: node_modules/@rescript/runtime/lib/ocaml (symlink)
+     - bin:     node_modules/.pnpm/@rescript+darwin-arm64@12.0.0-alpha.13/node_modules/@rescript/darwin-arm64/bin
+     - runtime: node_modules/.pnpm/node_modules/@rescript/runtime (symlink)
   *)
   | "bin" :: _platform :: "@rescript" :: "node_modules" :: _package :: ".pnpm"
     :: "node_modules" :: rest ->
-    build_path rest ["node_modules"; "@rescript"; "runtime"]
+    build_path rest
+      ["node_modules"; ".pnpm"; "node_modules"; "@rescript"; "runtime"]
   (* 2. Packages installed via npm
-     - bin:    node_modules/@rescript/{platform}/bin
-     - stdlib: node_modules/@rescript/runtime/lib/ocaml
+     - bin:     node_modules/@rescript/{platform}/bin
+     - runtime: node_modules/@rescript/runtime
   *)
   | "bin" :: _platform :: "@rescript" :: "node_modules" :: rest ->
     build_path rest ["node_modules"; "@rescript"; "runtime"]
   (* 3. Several other cases that can occur in local development, e.g.
-     - bin:    <repo>/packages/@rescript/{platform}/bin, <repo>/_build/install/default/bin
-     - stdlib: <repo>/packages/@rescript/runtime/lib/ocaml
+     - bin:     <repo>/packages/@rescript/{platform}/bin, <repo>/_build/install/default/bin
+     - runtime: <repo>/packages/@rescript/runtime
   *)
   | _ :: _ :: _ :: _ :: rest ->
     build_path rest ["packages"; "@rescript"; "runtime"]
