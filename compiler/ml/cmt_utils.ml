eda698033cb57b53c5750b07bc587049906edf0f
Gabriel Nordeborn <gabbe.nord@gmail.com>
2025-08-28 13:59:46 +0200

    rename migration template for pipe chains

--- compiler/ml/cmt_utils.ml
+++ compiler/ml/cmt_utils.ml
@@ -4,7 +4,7 @@ type deprecated_used = {
   source_loc: Location.t;
   deprecated_text: string;
   migration_template: Parsetree.expression option;
-  migration_piped_template: Parsetree.expression option;
+  migration_in_pipe_chain_template: Parsetree.expression option;
   context: deprecated_used_context option;
 }
 
@@ -13,14 +13,19 @@ type cmt_extra_info = {deprecated_used: deprecated_used list}
 let record_deprecated_used :
     (?deprecated_context:deprecated_used_context ->
     ?migration_template:Parsetree.expression ->
-    ?migration_piped_template:Parsetree.expression ->
+    ?migration_in_pipe_chain_template:Parsetree.expression ->
     Location.t ->
     string ->
     unit)
     ref =
   ref
     (fun
-      ?deprecated_context ?migration_template ?migration_piped_template _ _ ->
+      ?deprecated_context
+      ?migration_template
+      ?migration_in_pipe_chain_template
+      _
+      _
+    ->
       ignore deprecated_context;
       ignore migration_template;
-      ignore migration_piped_template)
+      ignore migration_in_pipe_chain_template)
