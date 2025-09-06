1b7ec6b0675bc94f11f7cc5d6da37373e483a248
Gabriel Nordeborn <gabbe.nord@gmail.com>
2025-09-06 14:16:06 +0200

    some tweaking of the structure, and explore an exact output mode

--- compiler/gentype/GentypeImportHelper.ml
+++ compiler/gentype/GentypeImportHelper.ml
@@ -1,3 +1,21 @@
+open GenTypeCommon
+
 let name = "$GenTypeImport"
 
 let alias = "type $GenTypeImport<Expected, T extends Expected> = T;"
+
+let strict_name = "$GenTypeImportStrict"
+
+let strict_alias =
+  "type $GenTypeImportStrict<T, Expected extends T> = Expected;"
+
+let rec should_inline_expected (t : type_) : bool =
+  match t with
+  | Ident {builtin = true; name; _} -> (
+    match name with
+    | "number" | "string" | "boolean" -> true
+    | _ -> false)
+  | Tuple inner -> inner |> List.for_all should_inline_expected
+  | Array (t, _) -> should_inline_expected t
+  | Promise t -> should_inline_expected t
+  | _ -> false
