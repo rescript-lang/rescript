3c99674c450002f34491e2721f09d647c26b0108
nojaf <florian.verdonck@outlook.com>
2025-09-12 11:16:42 +0200

    Add Promise.sleep to std

--- packages/@rescript/runtime/lib/es6/Stdlib_Promise.js
+++ packages/@rescript/runtime/lib/es6/Stdlib_Promise.js
@@ -6,7 +6,14 @@ function $$catch(promise, callback) {
   return promise.catch(err => callback(Primitive_exceptions.internalToException(err)));
 }
 
+function sleep(ms) {
+  return new Promise((resolve, param) => {
+    setTimeout(resolve, ms);
+  });
+}
+
 export {
   $$catch,
+  sleep,
 }
 /* No side effect */
--- packages/@rescript/runtime/lib/js/Stdlib_Promise.js
+++ packages/@rescript/runtime/lib/js/Stdlib_Promise.js
@@ -6,5 +6,12 @@ function $$catch(promise, callback) {
   return promise.catch(err => callback(Primitive_exceptions.internalToException(err)));
 }
 
+function sleep(ms) {
+  return new Promise((resolve, param) => {
+    setTimeout(resolve, ms);
+  });
+}
+
 exports.$$catch = $$catch;
+exports.sleep = sleep;
 /* No side effect */
