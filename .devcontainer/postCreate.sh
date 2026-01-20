ed32618b6f898f669614b82d1d1ab6342526540a
Paul Tsnobiladzé <paul.tsnobiladze@gmail.com>
2026-01-20 10:48:33 +0000

    install package too

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -3,7 +3,7 @@
 # Install dev dependencies from OPAM
 opam init -y --bare --disable-sandboxing
 opam switch create 5.3.0
-opam install . --deps-only --with-test --with-dev-setup -y
+opam install . --with-test --with-dev-setup -y
 
 # Add OPAM environment setup to shell startup script
 echo 'eval $(opam env)' >> ~/.zshrc
