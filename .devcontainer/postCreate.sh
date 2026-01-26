987ec933b6445090305bbe2f0de419124897f04e
tsnobip <dontshootthink@gmail.com>
2026-01-26 10:45:19 +0100

    install ocaml without switch

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# Add OPAM environment setup to shell startup script
-echo 'eval $(opam env)' >> ~/.zshrc
-echo 'eval $(opam env)' >> ~/.bashrc
-
-eval $(opam env)
-
 # Install dev dependencies from OPAM
 opam install . --with-test --with-dev-setup -y
 
