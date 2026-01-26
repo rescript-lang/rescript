72c45e4ef90f30a6e1fe0f5f52efe08f44b48630
tsnobip <dontshootthink@gmail.com>
2026-01-26 10:32:57 +0100

    set up opam in postcreate

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-# Install dev dependencies from OPAM
-opam install . --with-test --with-dev-setup -y
-
 # Add OPAM environment setup to shell startup script
 echo 'eval $(opam env)' >> ~/.zshrc
 echo 'eval $(opam env)' >> ~/.bashrc
+
+eval $(opam env)
+
+# Install dev dependencies from OPAM
+opam install . --with-test --with-dev-setup -y
 
 nvm install
 
