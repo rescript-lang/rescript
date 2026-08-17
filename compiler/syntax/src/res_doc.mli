0e5d0f0b719758fa643a3e77cb830b92303ddace
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2026-08-17 10:55:14 +0200

    Clarify compiler cleanup documentation
    Signed-off-by: Cristiano Calcagno <cristianoc@users.noreply.github.com>

--- compiler/syntax/src/res_doc.mli
+++ compiler/syntax/src/res_doc.mli
@@ -42,12 +42,13 @@ val equal : t
 val trailing_comma : t
 
 val will_break : t -> bool
-(** [will_break doc] checks whether [doc] contains forced line breaks.
+(** [will_break doc] checks whether [doc] contains forced line breaks. For each
+    [custom_layout], it only inspects the first alternative.
 
-    Forced breaks are not propagated through [customLayout], because doing so
+    Forced breaks are not propagated through [custom_layout], because doing so
     would always select the last layout the algorithm tries. Consumers can use
     [will_break] to detect a forced break in a custom layout and explicitly add
-    [breakParent] to propagate it to the parent document. *)
+    [break_parent] to propagate it to the parent document. *)
 
 val to_string : width:int -> t -> string
 val debug : t -> unit [@@live]
