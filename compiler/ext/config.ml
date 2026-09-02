c6dfcdedaec3a55178509d5aa7bd3295f4828e6f
Christoph Knittel <ck@cca.io>
2026-09-02 18:24:05 +0200

    Bump parsetree magic numbers
    Signed-off-by: Christoph Knittel <ck@cca.io>

--- compiler/ext/config.ml
+++ compiler/ext/config.ml
@@ -2,9 +2,9 @@ let cmi_magic_number = "Caml1999I029"
 
 (* Magic numbers for marshaled values of the *current* parsetree, whose layout
    changes across compiler versions. *)
-and ast_impl_magic_number = "ResImpl01303"
+and ast_impl_magic_number = "ResImpl01304"
 
-and ast_intf_magic_number = "ResIntf01303"
+and ast_intf_magic_number = "ResIntf01304"
 
 (* Magic numbers of the frozen Parsetree0 (OCaml 4.06) layout used on the
    external-PPX wire. They must never be written in front of a
