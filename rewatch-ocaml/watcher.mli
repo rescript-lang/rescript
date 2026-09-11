0fb88125486fe98876cd9d70aee1f07bae01ef31
Christoph Knittel <ck@cca.io>
2026-09-11 01:59:20 +0000

    Exercise watcher polling fallback
    Signed-off-by: Christoph Knittel <ck@cca.io>

--- rewatch-ocaml/watcher.mli
+++ rewatch-ocaml/watcher.mli
@@ -12,6 +12,19 @@ module For_test : sig
     change list
 
   val changes_are_incremental : change list -> bool
+
+  val run_with_native_failure :
+    message:string ->
+    on_fallback:(string -> unit) ->
+    root:string ->
+    prod:bool ->
+    features:string list option ->
+    filter:string option ->
+    clear_screen:bool ->
+    show_progress:bool ->
+    verbosity:int ->
+    build:(poll:(unit -> unit) -> changes:change list option -> build_result) ->
+    unit
 end
 
 val run :
