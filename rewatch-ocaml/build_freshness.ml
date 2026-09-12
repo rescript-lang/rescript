47eb5a20cfb6c9ea8ac8c9c89e76cce4635f6a5a
Christoph Knittel <ck@cca.io>
2026-09-12 19:40:52 +0000

    Support OCaml 5.0 option APIs
    Signed-off-by: Christoph Knittel <ck@cca.io>

--- rewatch-ocaml/build_freshness.ml
+++ rewatch-ocaml/build_freshness.ml
@@ -22,10 +22,11 @@ let source_is_not_older_than_ast compile_assets ~root ~source_mtimes path =
       in
       let belongs_to_source =
         try
-          (Ast_header.read published_ast).source
-          |> Option.exists (fun source ->
-              Platform.normalize_path_for_comparison source
-              = Platform.normalize_path_for_comparison absolute)
+          match (Ast_header.read published_ast).source with
+          | Some source ->
+            Platform.normalize_path_for_comparison source
+            = Platform.normalize_path_for_comparison absolute
+          | None -> false
         with Unix.Unix_error ((Unix.ENOENT | Unix.ENOTDIR), _, _) -> false
       in
       if belongs_to_source then File_util.modification_time published_ast
