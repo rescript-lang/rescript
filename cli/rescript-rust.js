e0df7b06ff07de28f23d206144f0064a68dbf313
Christoph Knittel <ck@cca.io>
2026-09-13 17:40:17 +0000

    Require the Rust rewatch binary
    Signed-off-by: Christoph Knittel <ck@cca.io>

--- cli/rescript-rust.js
+++ cli/rescript-rust.js
@@ -5,11 +5,4 @@
 import { rescript_rust_exe } from "./common/bins.js";
 import { runBuildSystem } from "./common/runBuildSystem.js";
 
-if (rescript_rust_exe === undefined) {
-  console.error(
-    "The Rust build-system binary is not available for this platform.",
-  );
-  process.exit(1);
-} else {
-  runBuildSystem(rescript_rust_exe);
-}
+runBuildSystem(rescript_rust_exe);
